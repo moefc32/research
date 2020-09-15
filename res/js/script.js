f576408553cdef73f8ec2adc92358b1d003d15e1
Faizal Chan <moefc32@gmail.com>
2020-09-16 01:51:44 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -20,3 +20,18 @@ function scrollIndicator() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("bar").style.width = scrolled + "%";
 }
+
+var channelID = "UCoxdB0_IJoOlYcLfiFkSbLg";
+var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID, function (data) {
+  for (let n = 0; n <= 3; n += 1) {
+    var x = n + 1;
+    var link = data.items[n].link;
+    var title = data.items[n].title;
+    var id = link.substr(link.indexOf("=") + 1);
+    $(".youtube-link" + x).attr("title", title);
+    $(".youtube-link" + x).attr("href", "https://www.youtube.com/watch?v=" + id);
+    $(".youtube-thumbnail" + x).attr("src", "http://img.youtube.com/vi/" + id + "/maxresdefault.jpg");
+    $(".youtube-title" + x).text(title);
+  }
+});
