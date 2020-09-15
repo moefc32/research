bf89650061bb4773afc4b12c8856d4eb4c746b1c
Faizal Chan <moefc32@gmail.com>
2020-09-16 00:17:24 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -1,11 +1,22 @@
 MediumWidget.Init({
   renderTo: '#medium-feed',
   params: {
-    "resource":"https://medium.com/risetnya-mfc",
-    "limit":5,
-    "fields":[
+    "resource": "https://medium.com/risetnya-mfc",
+    "limit": 5,
+    "fields": [
       "description",
       "publishAt"
     ]
   }
 })
+
+window.onscroll = function () {
+  scrollIndicator()
+};
+
+function scrollIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "%";
+}
