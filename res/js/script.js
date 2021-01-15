75b8427f7cdce0014ca812774977b0c0e0ae1fbf
Faizal Chan <moefc32@gmail.com>
2021-01-15 19:59:52 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -15,20 +15,20 @@ window.onscroll = function () {
 };
 
 function scrollIndicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("bar").style.width = scrolled + "%";
 }
 
-var channelID = "UCoxdB0_IJoOlYcLfiFkSbLg";
-var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+const channelID = "UCoxdB0_IJoOlYcLfiFkSbLg";
+const reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
 $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID, function (data) {
   for (let n = 0; n <= 4; n += 1) {
-    var x = n + 1;
-    var link = data.items[n].link;
-    var title = data.items[n].title;
-    var id = link.substr(link.indexOf("=") + 1);
+    let x = n + 1;
+    let link = data.items[n].link;
+    let title = data.items[n].title;
+    let id = link.substr(link.indexOf("=") + 1);
     $(".youtube-link" + x).attr("title", title);
     $(".youtube-link" + x).attr("href", "https://www.youtube.com/watch?v=" + id);
     $(".youtube-thumbnail" + x).attr("src", "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg");
