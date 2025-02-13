154894a081e5db9a53e8f0d9a456402b142f3ace
Faizal Chan <moefc32@gmail.com>
2025-02-13 18:53:33 +0700

    add license, update dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+	],
 	server: {
 		host: process.env.VITE_IPBIND || 'localhost',
-		port: parseInt(process.env.VITE_PORT) || 4000,
+		port: parseInt(process.env.VITE_PORT),
 	},
 });
