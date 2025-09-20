1ad66a548a1c79eef022b0b7d5d4606561684ed2
Faizal Chan. <moefc32@gmail.com>
2025-09-20 15:57:57 +0700

    minor edit banner

--- src/lib/component/Banner.svelte
+++ src/lib/component/Banner.svelte
@@ -1,6 +1,3 @@
-<script>
-</script>
-
 <section
     class="mb-5 px-3 pt-[120px] pb-5 bg-[linear-gradient(to_bottom_left,rgba(33,150,243,0.75),rgba(76,175,80,0.75))] bg-fixed text-white text-center h-[250px]"
 ></section>
